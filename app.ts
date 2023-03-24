ca0d13099938ce5272fc5acc6b13285f076ce4c7
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-03-25 01:16:06 +0700

    refactoring: serivces, app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,5 @@
-import * as cheerio from "cheerio";
 import express, { Request, Response } from "express";
-import { getHtmlData } from "./helper/html_parser";
-import { IMovie } from "./interface/Movie";
+import { getAllMovies } from "./services/Movies";
 
 const app = express();
 const port = 3000;
@@ -10,49 +8,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.get("/movies/:page?", async (req: Request, res: Response) => {
-  let url = "https://ngefilm21.shop/";
-  let $ = cheerio.load(await getHtmlData(url));
-
-  const paginate = $(".pagination");
-  let count_last_page: number = 0;
-
-  // Setter variable count_last_page
-  paginate.each((i, el) => {
-    // Remove next page button
-    $(el).find("a.next.page-numbers").remove();
-
-    // Get last page number paginate
-    count_last_page = parseInt($(el).find("a.page-numbers").last().text());
-  });
-
-  // Get page number from params
-  const page = parseInt(req.params.page);
-  page > 1
-    ? page <= count_last_page && (url = `https://ngefilm21.shop/page/${page}/`)
-    : url;
-
-  console.log(`🐢 You now in url: ${url}`);
-  $ = cheerio.load(await getHtmlData(url));
-
-  const movies: IMovie[] = [];
-  const container = $("article.item-infinite");
-  container.each((i, el) => {
-    movies.push({
-      title: $(el).find("h2.entry-title a").text(),
-      link: $(el).find("h2.entry-title a").attr("href"),
-      thumbnail_url: $(el).find("img.attachment-medium").attr("src"),
-      duration: $(el).find(".gmr-duration-item").text().trim(),
-      rating: $(el).find(".gmr-rating-item").text().trim(),
-      quality: $(el).find(".gmr-quality-item a").text() || "TV Show",
-      episode: $(el).find(".gmr-numbeps").text(),
-    });
-  });
-
-  res.status(200).json({
-    data: movies,
-  });
-});
+app.get("/movies/:page?", getAllMovies);
 
 app.listen(port, () => {
   console.log(`🚀 App listening at http://localhost:${port}`);
---
+++ services/Movies.ts
@@ -0,0 +1,48 @@
+import * as cheerio from "cheerio";
+import { Request, Response } from "express";
+import { getHtmlData } from "../helper/html_parser";
+import { IMovie } from "../interface/IMovie";
+
+export async function getAllMovies(req: Request, res: Response) {
+  let url = "https://ngefilm21.shop/";
+  let $ = cheerio.load(await getHtmlData(url));
+
+  const paginate = $(".pagination");
+  let count_last_page: number = 0;
+
+  // Setter variable count_last_page
+  paginate.each((i, el) => {
+    // Remove next page button
+    $(el).find("a.next.page-numbers").remove();
+
+    // Get last page number paginate
+    count_last_page = parseInt($(el).find("a.page-numbers").last().text());
+  });
+
+  // Get page number from params
+  const page = parseInt(req.params.page);
+  page > 1
+    ? page <= count_last_page && (url = `https://ngefilm21.shop/page/${page}/`)
+    : url;
+
+  console.log(`🐢 You now in url: ${url}`);
+  $ = cheerio.load(await getHtmlData(url));
+
+  const movies: IMovie[] = [];
+  const container = $("article.item-infinite");
+  container.each((i, el) => {
+    movies.push({
+      title: $(el).find("h2.entry-title a").text(),
+      link: $(el).find("h2.entry-title a").attr("href"),
+      thumbnail_url: $(el).find("img.attachment-medium").attr("src"),
+      duration: $(el).find(".gmr-duration-item").text().trim(),
+      rating: $(el).find(".gmr-rating-item").text().trim(),
+      quality: $(el).find(".gmr-quality-item a").text() || "TV Show",
+      episode: $(el).find(".gmr-numbeps").text(),
+    });
+  });
+
+  res.status(200).json({
+    data: movies,
+  });
+};
