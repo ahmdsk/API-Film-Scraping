3fc3009906db71c3fb174d1907a0704044e31fd9
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-03-25 14:01:34 +0700

    add: TV Movies

--- interface/Movie.ts
+++ interface/Movie.ts
@@ -10,19 +10,28 @@ export interface IMovie {
 }
 
 export interface IContainerMovieData {
-  Tagline: string | undefined,
-  Rating: string | undefined,
-  Genre: string | undefined,
-  Kualitas: string | undefined,
-  Tahun: string | undefined,
-  Durasi: string | undefined,
-  Negara: string | undefined,
-  Rilis: string | undefined,
-  Bahasa: string | undefined,
-  Direksi: string | undefined,
-  Pemain: string | undefined,
+  Tagline: string | undefined;
+  Rating: string | undefined;
+  Genre: string | undefined;
+  Kualitas: string | undefined;
+  Tahun: string | undefined;
+  Durasi: string | undefined;
+  Negara: string | undefined;
+  Rilis: string | undefined;
+  Bahasa: string | undefined;
+  Direksi: string | undefined;
+  Pemain: string | undefined;
 }
 
+export interface IContainerTVData {
+  Genre: string | undefined;
+  Tahun: string | undefined;
+  Negara: string | undefined;
+  Rilis: string | undefined;
+  Episode: string | undefined;
+  Jaringan: string | undefined;
+  Pemain: string | undefined;
+}
 
 export interface MovieData {
   title: string;
