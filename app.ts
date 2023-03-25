18051531706854f31ec9ad1e21e4f4965d786fc0
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-03-25 14:54:18 +0700

    add: response in root routing

--- app.ts
+++ app.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from "express";
 import { getAllMovies, getDetailMovie } from "./src/services/Movies";
 import { getDetailTV, getDetailEpisodeTV } from "./src/services/TV";
+import { responseSuccessWithMessage } from "./src/utils/Response";
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.status(200).json(responseSuccessWithMessage("Welcome to API, please read the documentation for more information about this API in this https://github.com/ahmdsk/API-Film-Scraping. Thank you!"));
 });
 
 app.get("/movies/:page?", getAllMovies);
