b32f7aec64f035b87518544c7951589899119316
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-03-25 21:00:36 +0700

    add: cors and fix num of paginate

--- app.ts
+++ app.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express";
 import { getAllMovies, getDetailMovie } from "./src/services/Movies";
 import { getDetailTV, getDetailEpisodeTV } from "./src/services/TV";
 import { responseSuccessWithMessage } from "./src/utils/Response";
-
+import cors from "cors"
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json(responseSuccessWithMessage("Welcome to API, please read the documentation for more information about this API in this https://github.com/ahmdsk/API-Film-Scraping. Thank you!"));
--- src/services/Movies.ts
+++ src/services/Movies.ts
@@ -14,50 +14,60 @@ import {
 } from "../utils/Response";
 
 export async function getAllMovies(req: Request, res: Response) {
-  let url = "https://ngefilm21.shop/";
-  let $ = cheerio.load(await getHtmlData(url));
+  try {
+    let url = "https://ngefilm21.shop/";
+    let $ = cheerio.load(await getHtmlData(url));
 
-  const paginate = $(".pagination");
-  let count_last_page: number = 0;
+    const paginate = $(".pagination");
+    let count_last_page: number = 0;
 
-  // Setter variable count_last_page
-  paginate.each((i, el) => {
-    // Remove next page button
-    $(el).find("a.next.page-numbers").remove();
+    // Setter variable count_last_page
+    paginate.each((i, el) => {
+      // Remove next page button
+      $(el).find("a.next.page-numbers").remove();
 
-    // Get last page number paginate
-    count_last_page = parseInt($(el).find("a.page-numbers").last().text());
-  });
-
-  // Get page number from params
-  const page = parseInt(req.params.page);
-  page > 1
-    ? page <= count_last_page
-      ? (url = `https://ngefilm21.shop/page/${page}/`)
-      : res.status(404).json(responseErrorWithMessage("Page Not Found"))
-    : url;
-
-  console.log(`🐢 You now in url: ${url}`);
-  $ = cheerio.load(await getHtmlData(url));
-
-  const movies: IMovie[] = [];
-  const container = $("article.item-infinite");
-  container.each((i, el) => {
-    let link = $(el).find("h2.entry-title a").attr("href");
-
-    movies.push({
-      title: $(el).find("h2.entry-title a").text(),
-      slug: link?.replace(/^https:\/\/ngefilm21\.shop/, ""),
-      thumbnail_url: $(el).find("img.attachment-medium").attr("src"),
-      duration: $(el).find(".gmr-duration-item").text().trim(),
-      rating: $(el).find(".gmr-rating-item").text().trim(),
-      quality: $(el).find(".gmr-quality-item a").text() || "TV Show",
-      episode: $(el).find(".gmr-numbeps").text(),
-      link,
+      // Get last page number paginate
+      count_last_page = parseInt($(el).find("a.page-numbers").last().text());
     });
-  });
 
-  res.status(200).json(responseSuccessWithData(movies));
+    // Get page number from params
+    const page = parseInt(req.params.page);
+    page > 1
+      ? page <= count_last_page
+        ? (url = `https://ngefilm21.shop/page/${page}/`)
+        : res.status(404).json(responseErrorWithMessage("Page Not Found"))
+      : url;
+
+    console.log(`🐢 You now in url: ${url}`);
+    $ = cheerio.load(await getHtmlData(url));
+
+    const movies: IMovie[] = [];
+    const container = $("article.item-infinite");
+    container.each((i, el) => {
+      let link = $(el).find("h2.entry-title a").attr("href");
+
+      movies.push({
+        title: $(el).find("h2.entry-title a").text(),
+        slug: link?.replace(/^https:\/\/ngefilm21\.shop/, ""),
+        thumbnail_url: $(el).find("img.attachment-medium").attr("src"),
+        duration: $(el).find(".gmr-duration-item").text().trim(),
+        rating: $(el).find(".gmr-rating-item").text().trim(),
+        quality: $(el).find(".gmr-quality-item a").text() || "TV Show",
+        episode: $(el).find(".gmr-numbeps").text(),
+        link,
+      });
+    });
+
+    res.status(200).json(
+      responseSuccessWithData({
+        total_page: count_last_page,
+        movies,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(responseErrorWithMessage());
+  }
 }
 
 export async function getDetailMovie(req: Request, res: Response) {
@@ -139,7 +149,7 @@ export async function getDetailMovie(req: Request, res: Response) {
       Direksi: undefined,
       Pemain: undefined,
       Tagline: undefined,
-      Rating: undefined
+      Rating: undefined,
     };
 
     for (let i = 2; i < key_movie_data.length; i++) {
@@ -170,20 +180,22 @@ export async function getDetailMovie(req: Request, res: Response) {
 
     // get stream links
     const stream_links = [];
-    const num_of_stream_links = $('ul.muvipro-player-tabs li:last-child a').text().split(' ').at(-1);
-    if(num_of_stream_links) {
-      for(let i = 2; i <= parseInt(num_of_stream_links); i++) {
-        stream_links.push(url + slug + '/?player=' + i)
+    const num_of_stream_links = $("ul.muvipro-player-tabs li:last-child a")
+      .text()
+      .split(" ")
+      .at(-1);
+    if (num_of_stream_links) {
+      for (let i = 2; i <= parseInt(num_of_stream_links); i++) {
+        stream_links.push(url + slug + "/?player=" + i);
       }
     }
 
-
     const streaming_links = [];
-    for(let i = 0; i < stream_links.length; i++) {
+    for (let i = 0; i < stream_links.length; i++) {
       const $ = cheerio.load(await getHtmlData(stream_links[i]));
-      const stream_link = $('.gmr-embed-responsive iframe').attr('src');
+      const stream_link = $(".gmr-embed-responsive iframe").attr("src");
 
-      streaming_links.push(stream_link)
+      streaming_links.push(stream_link);
     }
 
     res.status(200).json(
@@ -192,20 +204,20 @@ export async function getDetailMovie(req: Request, res: Response) {
         thumbnail_url: movie_data[0].thumbnail_url,
         description: detail_movie[0].description,
         created_at: detail_movie[0].created_at,
-        tagline: container_movie_data.Tagline ?? '',
-        rating: container_movie_data.Rating ?? '',
-        genre: container_movie_data.Genre ?? '',
-        quality: container_movie_data.Kualitas ?? '',
-        year: container_movie_data.Tahun ?? '',
-        duration: container_movie_data.Durasi ?? '',
-        country: container_movie_data.Negara ?? '',
-        realease: container_movie_data.Rilis ?? '',
-        language: container_movie_data.Bahasa ?? '',
-        director: container_movie_data.Direksi ?? '',
-        artist: container_movie_data.Pemain ?? '',
+        tagline: container_movie_data.Tagline ?? "",
+        rating: container_movie_data.Rating ?? "",
+        genre: container_movie_data.Genre ?? "",
+        quality: container_movie_data.Kualitas ?? "",
+        year: container_movie_data.Tahun ?? "",
+        duration: container_movie_data.Durasi ?? "",
+        country: container_movie_data.Negara ?? "",
+        realease: container_movie_data.Rilis ?? "",
+        language: container_movie_data.Bahasa ?? "",
+        director: container_movie_data.Direksi ?? "",
+        artist: container_movie_data.Pemain ?? "",
         voting: detail_voting[0],
         download_links,
-        streaming_links
+        streaming_links,
       })
     );
   } catch (error) {
